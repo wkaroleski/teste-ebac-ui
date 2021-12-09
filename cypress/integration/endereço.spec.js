9c41aaf8da9595e8e3adceef0e647e58d1ea433f
WagnerKaroleski <wagnerkaroleski@pop-os.localdomain>
2021-12-09 17:45:15 -0300

    Aterações endereço com lista

--- cypress/integration/endereço.spec.js
+++ cypress/integration/endereço.spec.js
@@ -1,5 +1,6 @@
 /// <reference types="cypress" />
 import EnderecoPage from '../support/page-objects/endereço.page'
+const dadosEndereco = require('../fixtures/endereco.json')
 
 
 describe('Funcionalidade endereços - faturamento e entrega', () => {
@@ -11,7 +12,24 @@ describe('Funcionalidade endereços - faturamento e entrega', () => {
          
     });
 
-    it.only('Deve fazer cadastro de faturamento com sucesso', () => {
+    it('Deve fazer cadastro de faturamento com sucesso', () => {
        EnderecoPage.editarEnderecoFaturamento('Fernanda', 'Petry', 'DCA', 'Brasil', 'Rua tamandare', '1313', 'Pelotas', 'Roraima', '54678974', '67943990673', 'fefe@teste.com')
     });
+
+    it('Deve fazer cadastro de faturamento com sucesso - usando arquivo de dados', () => {
+        EnderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[2].nome,
+            dadosEndereco[2].sobrenome,
+            dadosEndereco[2].empresa,
+            dadosEndereco[2].pais,
+            dadosEndereco[2].endereco,
+            dadosEndereco[2].numero,
+            dadosEndereco[2].cidade,
+            dadosEndereco[2].estado,
+            dadosEndereco[2].cep,
+            dadosEndereco[2].telefone,
+            dadosEndereco[2].email
+            )
+
+     });
 });
